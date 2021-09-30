1aed5d99297f9dcf1039dad75fb0e8f480f5c47a
Samikmalhotra <samikmalhotra66@gmail.com>
2021-09-30 22:48:48 +0530

    designed navbar, login and register page

--- client/src/components/Pages/Login.js
+++ client/src/components/Pages/Login.js
@@ -2,49 +2,67 @@ import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../context/auth/AuthContext';
 import ErrorContext from '../../context/error/ErrorContext';
-
+import './Auth.css';
 const Login = (props) => {
-    const authContext = useContext(AuthContext);
-    const errorContext = useContext(ErrorContext);
-    const { isAuthenticated, login } = authContext;
-    const { error } = errorContext;
+  const authContext = useContext(AuthContext);
+  const errorContext = useContext(ErrorContext);
+  const { isAuthenticated, login } = authContext;
+  const { error } = errorContext;
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            props.history.push('/');
-        }
-        // eslint-disable-next-line
-    }, [props.history, isAuthenticated])
-
-
-    const [email, Setemail] = useState("");
-    const [password, Setpassword] = useState("");
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-            email,
-            password
-        }
-        login(data);
-        // context
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
     }
-    const onEmailChange = (e) => {
-        Setemail(e.target.value);
-    }
-    const onPasswordChange = (e) => {
-        Setpassword(e.target.value);
-    }
-    return (
-        <div>
-            {error && <p>{error}</p>}
-            <form onSubmit={onSubmit}>
-                <input value={email} type="text" placeholder="Email" onChange={onEmailChange} />
-                <input value={password} type="password" placeholder="Password" onChange={onPasswordChange} />
-                <input type="submit" value="Login" />
-                <Link to='/register'>Don't have an account? Register</Link>
-            </form>
-        </div>
-    )
-}
+    // eslint-disable-next-line
+  }, [props.history, isAuthenticated]);
 
-export default Login
+  const [email, Setemail] = useState('');
+  const [password, Setpassword] = useState('');
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email,
+      password,
+    };
+    login(data);
+    // context
+  };
+  const onEmailChange = (e) => {
+    Setemail(e.target.value);
+  };
+  const onPasswordChange = (e) => {
+    Setpassword(e.target.value);
+  };
+  return (
+    <div className='auth-page'>
+      <div className='auth-form'>
+        {error && <p className='error'>{error}</p>}
+
+        <h1>Login</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            value={email}
+            type='text'
+            placeholder='Email'
+            onChange={onEmailChange}
+          />
+          <input
+            value={password}
+            type='password'
+            placeholder='Password'
+            onChange={onPasswordChange}
+          />
+          <center>
+            <button type='submit'>Login</button>
+          </center>
+          <Link to='/register'>
+            <p className='link'>Don't have an account? Register</p>
+          </Link>
+        </form>
+      </div>
+      <div className='space-10' />
+    </div>
+  );
+};
+
+export default Login;
--- client/src/components/Pages/Register.js
+++ client/src/components/Pages/Register.js
@@ -4,57 +4,84 @@ import AuthContext from '../../context/auth/AuthContext';
 import ErrorContext from '../../context/error/ErrorContext';
 
 const Register = (props) => {
-    const authContext = useContext(AuthContext);
-    const errorContext = useContext(ErrorContext);
-    const { isAuthenticated, register } = authContext;
-    const { error } = errorContext;
+  const authContext = useContext(AuthContext);
+  const errorContext = useContext(ErrorContext);
+  const { isAuthenticated, register } = authContext;
+  const { error } = errorContext;
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            props.history.push('/');
-        }
-    }, [props.history, isAuthenticated])
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+  }, [props.history, isAuthenticated]);
 
-    const [email, Setemail] = useState("");
-    const [password, Setpassword] = useState("");
-    const [name, Setname] = useState("");
-    const [username, Setusername] = useState("");
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-            name,
-            username,
-            email,
-            password
-        }
-        register(data);
-        // context
-    }
-    const onEmailChange = (e) => {
-        Setemail(e.target.value);
-    }
-    const onPasswordChange = (e) => {
-        Setpassword(e.target.value);
-    }
-    const onNameChange = (e) => {
-        Setname(e.target.value);
-    }
-    const onUserNameChange = (e) => {
-        Setusername(e.target.value);
-    }
-    return (
-        <div>
-            {error && <p>{error}</p>}
-            <form onSubmit={onSubmit}>
-                <input value={name} type="text" placeholder="Name" autoFocus onChange={onNameChange} />
-                <input value={username} type="text" placeholder="Username" onChange={onUserNameChange} />
-                <input value={email} type="text" placeholder="Email" onChange={onEmailChange} />
-                <input value={password} type="password" placeholder="Password" onChange={onPasswordChange} />
-                <input type="submit" value="Register" />
-                <Link to='/login'>Have an account? Login</Link>
-            </form>
-        </div>
-    )
-}
+  const [email, Setemail] = useState('');
+  const [password, Setpassword] = useState('');
+  const [name, Setname] = useState('');
+  const [username, Setusername] = useState('');
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      name,
+      username,
+      email,
+      password,
+    };
+    register(data);
+    // context
+  };
+  const onEmailChange = (e) => {
+    Setemail(e.target.value);
+  };
+  const onPasswordChange = (e) => {
+    Setpassword(e.target.value);
+  };
+  const onNameChange = (e) => {
+    Setname(e.target.value);
+  };
+  const onUserNameChange = (e) => {
+    Setusername(e.target.value);
+  };
+  return (
+    <div className='auth-page'>
+      <div className='auth-form'>
+        {error && <p className='error'>{error}</p>}
+        <h1>Register</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            value={name}
+            type='text'
+            placeholder='Name'
+            autoFocus
+            onChange={onNameChange}
+          />
+          <input
+            value={username}
+            type='text'
+            placeholder='Username'
+            onChange={onUserNameChange}
+          />
+          <input
+            value={email}
+            type='text'
+            placeholder='Email'
+            onChange={onEmailChange}
+          />
+          <input
+            value={password}
+            type='password'
+            placeholder='Password'
+            onChange={onPasswordChange}
+          />
+          <button type='submit'>Register</button>
+          <Link to='/login'>
+            <p>Have an account? Login</p>
+          </Link>
+        </form>
+      </div>
+      <div className='space-10' />
+    </div>
+  );
+};
 
-export default Register
+export default Register;
